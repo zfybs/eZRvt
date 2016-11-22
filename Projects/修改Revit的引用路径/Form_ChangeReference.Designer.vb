1d8781ae60c8aed6e7d6841116f4a4d79c53e8f8
zengfy <zengfy@DESKTOP-ZENGFY>
2016-11-22 19:23:05 +0800

    将新的项目“修改Revit的引用路径”添加到解决方案中

---
+++ Projects/修改Revit的引用路径/Form_ChangeReference.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_ChangeReference
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form_ChangeReference))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox_RevitPath = New System.Windows.Forms.TextBox()
+        Me.btnChooseDirectory = New System.Windows.Forms.Button()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox_SolutionPath = New System.Windows.Forms.TextBox()
+        Me.btnChooseFile = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnOpenSolution = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 333)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(125, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Revit.dll 文件夹路径"
+        '
+        'TextBox_RevitPath
+        '
+        Me.TextBox_RevitPath.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox_RevitPath.Location = New System.Drawing.Point(14, 349)
+        Me.TextBox_RevitPath.Name = "TextBox_RevitPath"
+        Me.TextBox_RevitPath.Size = New System.Drawing.Size(514, 21)
+        Me.TextBox_RevitPath.TabIndex = 1
+        '
+        'btnChooseDirectory
+        '
+        Me.btnChooseDirectory.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnChooseDirectory.Location = New System.Drawing.Point(532, 349)
+        Me.btnChooseDirectory.Name = "btnChooseDirectory"
+        Me.btnChooseDirectory.Size = New System.Drawing.Size(75, 23)
+        Me.btnChooseDirectory.TabIndex = 2
+        Me.btnChooseDirectory.Text = "选择"
+        Me.btnChooseDirectory.UseVisualStyleBackColor = True
+        '
+        'btnOk
+        '
+        Me.btnOk.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnOk.Location = New System.Drawing.Point(532, 384)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 3
+        Me.btnOk.Text = "修改"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(179, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "项目文件及引用的Revit.dll路径"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 12)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = ".sln 文件"
+        '
+        'TextBox_SolutionPath
+        '
+        Me.TextBox_SolutionPath.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox_SolutionPath.Name = "TextBox_SolutionPath"
+        Me.TextBox_SolutionPath.Size = New System.Drawing.Size(514, 21)
+        Me.TextBox_SolutionPath.TabIndex = 1
+        '
+        'btnChooseFile
+        '
+        Me.btnChooseFile.Location = New System.Drawing.Point(532, 25)
+        Me.btnChooseFile.Name = "btnChooseFile"
+        Me.btnChooseFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnChooseFile.TabIndex = 2
+        Me.btnChooseFile.Text = "选择"
+        Me.btnChooseFile.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnOpenSolution
+        '
+        Me.btnOpenSolution.Location = New System.Drawing.Point(532, 66)
+        Me.btnOpenSolution.Name = "btnOpenSolution"
+        Me.btnOpenSolution.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpenSolution.TabIndex = 6
+        Me.btnOpenSolution.Text = "打开"
+        Me.ToolTip1.SetToolTip(Me.btnOpenSolution, "打开此.sln文件所在的文件夹中所有的Project，并搜索其中的VB或C#项目文件")
+        Me.btnOpenSolution.UseVisualStyleBackColor = True
+
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(16, 95)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(512, 212)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.WordWrap = False
+        '
+        'Form_ChangeReference
+        '
+        Me.AcceptButton = Me.btnOk
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 419)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnOpenSolution)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.btnChooseFile)
+        Me.Controls.Add(Me.btnChooseDirectory)
+        Me.Controls.Add(Me.TextBox_SolutionPath)
+        Me.Controls.Add(Me.TextBox_RevitPath)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form_ChangeReference"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "修改引用"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_RevitPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnChooseDirectory As System.Windows.Forms.Button
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_SolutionPath As System.Windows.Forms.TextBox
+    Friend WithEvents btnChooseFile As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents btnOpenSolution As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+
+End Class
---
+++ Projects/修改Revit的引用路径/Form_ChangeReference.vb
@@ -0,0 +1,239 @@
+﻿Imports System.Xml
+Imports System.IO
+Imports RefreshRevitReference.RefProject
+Public Class Form_ChangeReference
+
+#Region "  ---  Properties"
+
+
+    Private F_SolutionFile As String
+    ''' <summary> .sln 文件的路径</summary>
+    Public Property SolutionFile As String
+        Get
+            Return F_SolutionFile
+        End Get
+        Set(value As String)
+            blnSolutionOpened = False
+            F_SolutionFile = value
+        End Set
+    End Property
+
+    ''' <summary> 要重新引用的Revit径</summary>
+    Public Property NewRefPath As String
+
+    Private F_Projects As New List(Of RefProject)
+    Private WriteOnly Property Projects As List(Of RefProject)
+        Set(value As List(Of RefProject))
+            '
+            With TextBox1
+                .Clear()
+                '
+                Dim index As Integer
+                index = (New FileInfo(SolutionFile)).Directory.FullName.Length
+
+                '
+                Dim c As Integer = value.Count
+                Dim p As RefProject
+                Dim ProjectFile As String
+                For i = 0 To c - 1
+                    p = value.Item(i)
+                    ProjectFile = p.ProjectFilePath.Substring(index)
+                    .AppendText("---------------------------  " & i + 1 & " ----------------------------" & vbCrLf &
+                              ProjectFile & vbCrLf &
+                            "RevitAPI.dll: " & p.RevitDllPath & vbCrLf & vbCrLf)
+                Next
+            End With
+            ' 一定要最后再进行刷新
+            F_Projects = value
+        End Set
+    End Property
+
+#End Region
+
+#Region "  ---  Fields"
+
+    Private blnProjectsSaved As Boolean = True
+    '
+    Private F_blnSolutionOpened As Boolean
+    Private Property blnSolutionOpened As Boolean
+        Get
+            Return Me.F_blnSolutionOpened
+        End Get
+        Set(value As Boolean)
+            Me.F_blnSolutionOpened = value
+            If value = True Then
+                btnOpenSolution.Enabled = False
+            Else
+                btnOpenSolution.Enabled = True
+            End If
+        End Set
+    End Property
+
+    ''' <summary> 项目文件中引用的Revit的文件夹路径 </summary>
+    Private OriginalRefPath As String
+
+#End Region
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+
+        ' 绑定数据
+        TextBox_SolutionPath.DataBindings.Add("Text", Me, "SolutionFile", False, DataSourceUpdateMode.OnPropertyChanged)
+        TextBox_RevitPath.DataBindings.Add("Text", Me, "NewRefPath", False, DataSourceUpdateMode.OnPropertyChanged)
+        '
+        '设置初始路径
+        TextBox_RevitPath.Text = My.Settings.LastDllPath
+        TextBox_SolutionPath.Text = My.Settings.LastSolutionPath
+    End Sub
+    ''
+    Private Function OpenSolution() As Boolean Handles btnOpenSolution.Click
+        Dim blnSucceed As Boolean = True
+        If Not blnSolutionOpened Then
+            Dim pros As New List(Of RefProject)
+            Dim strPro As String = Nothing
+            Dim files As FileInfo()
+            Dim ProsDir As New DirectoryInfo(Path.Combine(New FileInfo(SolutionFile).Directory.FullName, "Projects"))
+            If Directory.Exists(ProsDir.FullName) Then
+                For Each d As DirectoryInfo In ProsDir.GetDirectories
+                    files = d.GetFiles("*.vbproj")
+                    If files.Length > 0 Then
+                        strPro = files(0).FullName
+                    Else
+                        files = d.GetFiles("*.csproj")
+                        If files.Length > 0 Then
+                            strPro = files(0).FullName
+                        End If
+                    End If
+                    '
+                    If strPro IsNot Nothing Then
+                        Dim doc As New XmlDocument
+                        doc.Load(strPro)
+                        Dim p As New RefProject(doc, strPro)
+                        pros.Add(p)
+                        '
+                        p.GetRevitPath()
+                    End If
+                Next
+                blnSolutionOpened = True
+                ' blnProjectsSaved = False  ' 在打开solution的操作中，并没有进行写入操作，所以并不需要保存
+            Else
+                MessageBox.Show("未找到对应的项目文件夹！", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                blnSucceed = False
+            End If
+            Me.Projects = pros
+        End If
+        Return blnSucceed
+    End Function
+
+    Private Sub SaveAll()
+        For Each p In F_Projects
+            p.Save()
+        Next
+        Me.blnProjectsSaved = True
+    End Sub
+
+#Region "  ---  事件"
+
+    ' 修改引用路径
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        '
+        Dim blnSucceed As Boolean = True
+        If Not blnSolutionOpened Then
+            blnSucceed = OpenSolution()
+        End If
+
+        '
+        For Each pro As RefProject In F_Projects
+            pro.ChangeRef(Me.TextBox_RevitPath.Text)
+        Next
+        blnProjectsSaved = False
+        '
+        If blnSucceed Then
+            Dim res As DialogResult = MessageBox.Show("引用路径修改成功，是否保存？", "Congratulations!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)
+            If res = Windows.Forms.DialogResult.OK Then
+                SaveAll()
+                Me.btnOpenSolution.Enabled = True
+            End If
+        End If
+        '
+        blnSolutionOpened = False
+        My.Settings.LastDllPath = TextBox_RevitPath.Text
+        My.Settings.LastSolutionPath = TextBox_SolutionPath.Text
+    End Sub
+
+    ' 选择文件夹路径或者选择项目文件路径
+    Private Sub btnChooseDirectory_Click(sender As Object, e As EventArgs) Handles btnChooseDirectory.Click
+        With Me.FolderBrowserDialog1
+            .ShowNewFolderButton = True
+            .Description = "Revit.exe 所在的文件夹。"
+            '
+            If .ShowDialog() = Windows.Forms.DialogResult.OK Then
+                TextBox_RevitPath.Text = .SelectedPath
+            End If
+
+        End With
+    End Sub
+    Private Sub btnChooseFile_Click(sender As Object, e As EventArgs) Handles btnChooseFile.Click
+        With Me.OpenFileDialog1
+            .Multiselect = False
+            .Title = "Visual Studio 解决方案"
+            .Filter = "项目(*.sln)|*.sln|所有文件(*.*)|*.*"
+            '
+            If .ShowDialog() = Windows.Forms.DialogResult.OK Then
+                TextBox_SolutionPath.Text = .FileName
+            End If
+
+        End With
+    End Sub
+
+    ' 关闭窗口
+    Private Sub Form_ChangeReference_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If Not blnProjectsSaved Then
+            Dim res As DialogResult = MessageBox.Show("是否保存对文档的修改？", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1)
+            If res = Windows.Forms.DialogResult.OK Then
+                SaveAll()
+            End If
+        End If
+        '
+        Me.btnOpenSolution.Enabled = True
+    End Sub
+
+#Region "  ---  拖拽项目文件"
+
+    '拖拽操作
+    Private Sub APPLICATION_MAINFORM_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        ' See if the data includes text.
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            ' There is text. Allow copy.
+            e.Effect = DragDropEffects.Copy
+        Else
+            ' There is no text. Prohibit drop.
+            e.Effect = DragDropEffects.None
+        End If
+
+    End Sub
+
+    Private Sub APPLICATION_MAINFORM_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        Dim FileDrop As String() = e.Data.GetData(DataFormats.FileDrop)
+        ' DoSomething with the Files or Directories that are droped in.
+        Dim filepath As String = FileDrop(0)
+        Dim ext As String = Path.GetExtension(filepath)
+
+        ' If String.Compare(ext, ".vbproj", True) = 0 OrElse (String.Compare(ext, ".csproj", True) = 0) Then
+        If String.Compare(ext, ".sln", True) = 0 Then
+            If String.Compare(filepath, Me.TextBox_SolutionPath.Text, True) <> 0 Then
+                Me.TextBox_SolutionPath.Text = filepath
+            End If
+
+        Else
+            MessageBox.Show("所选择的文件不是Visual Studio的 .sln 文件。", _
+                             "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+#End Region
+
+#End Region
+
+End Class
